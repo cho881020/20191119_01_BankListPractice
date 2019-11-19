ba3be90746c458f599b03561f0dae881e7651105
조경진 <kj_cho@idealidea.co.kr>
2019-11-19 20:20:16 +0900

    API 호출 => 은행 목록 뽑아내기 직전

--- app/src/main/java/com/tjoeun/a20191119_01_banklistpractice/datas/Bank.kt
+++ app/src/main/java/com/tjoeun/a20191119_01_banklistpractice/datas/Bank.kt
@@ -1,8 +1,23 @@
 package com.tjoeun.a20191119_01_banklistpractice.datas
 
+import org.json.JSONObject
+
 class Bank {
     var id = 0
     var code = ""
     var name = ""
     var logo = ""
+
+    companion object {
+
+        fun getBankFromJsonObject(json:JSONObject) : Bank {
+            val bank = Bank()
+
+//            파싱 코드들 작성
+
+            return bank
+
+        }
+
+    }
 }
